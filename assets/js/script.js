9848dd39bb8a3e3c3d027dc3aa7043652e103eaf
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-09-19 02:14:45 -0500

    Created base logic in javaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,49 @@ var today = moment().format("dddd, MMMM Do");
 var todayHour = moment().format("hha");
 $('#currentDay').text(today);
 
+
+
+
 // Set the hours for the time column
 // .hour lets you get or set the hour
-console.log(moment().format("hha"));
-var fiveAmTxt = $('#h4Txt').text(moment().hour("8").format("hha"));
-var fiveAmTxt = $('#h5Txt').text(moment().hour("8").format("hha"));
-var sixAmTxt = $('#h6Txt').text(moment().hour("6").format("hha"));
-var sevenAmTxt = $('#h7Txt').text(moment().hour("13").format("hha"));
+$('#h4Txt').text(moment().hour("0").format("hha"));
+$('#h6Txt').text(moment().hour("3").format("hha"));
+$('#h7Txt').text(moment().hour("13").format("hha"));
+$('#h5Txt').text(moment().hour("1").format("hha"));
+// This could be made into a loop
+
+var fiveAmTxt = moment().hour("2").format("hha"); 
+
+var todayHour2 = moment().hour("4").format("hha"); 
+console.log("Today's Date: " + today)
+console.log("Current Hour: " + todayHour)
 
 
+console.log("Block Hour: " + fiveAmTxt)
+function hour () {
+    console.log("Timer Test")
+    if (todayHour2 < fiveAmTxt) {
+        console.log("The time has passed")
+    } else if (todayHour2 == fiveAmTxt) {
+        console.log("It's the current hour")
+    } else {
+        console.log("The time is in the future")
+    }
+    return;
+};
 
+// Execute every 5 minutes
+// setInterval(hour, 1000)
+hour()
 
+function testLocalStorageRecall() {
+    var testRecall = localStorage.getItem("testLocal")
+    console.log("Prior Value: " + testRecall)
+    // How can you pass the value back into the empty form? 
+    // $('#test-form').text(testRecall)
+}
+
+testLocalStorageRecall(); 
 
 // var test = $('#test').val();
 var test = $('input[name="test"]'); 
@@ -21,10 +53,12 @@ var testBtn = $('#test-form');
 function testSubmit(event) {
     event.preventDefault();
     console.log(test.val());
-    // $('input[name="test"]').val("");
+    localStorage.setItem("testLocal",test.val());
 }
 
 testBtn.on('submit', testSubmit);
+
+
 
 // var fiveAmSchedule = $("#id").val();
 var fiveAmSchedule = $('input[name="5am"]').val();
