c70926bb31f8b7a10de66958aec67d47c9da0aa9
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-09-20 20:36:03 -0500

    Updates w/ iterations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,27 @@
 var today = moment().format("dddd, MMMM Do");
-var todayHour = moment().format("h a");
+var todayHour = moment().format("HH");
 $('#currentDay').text(today);
 
 LocalStorageRecall(); 
 
 // Time stamps for logic to determine if the time block is past, present, or future
-var fiveAmTxt = moment().hour("5").format("h a"); 
-var sixAmTxt = moment().hour("6").format("h a");
-var sevenAmTxt = moment().hour("7").format("h a");
-var eightAmTxt = moment().hour("8").format("h a");
-var nineAmTxt = moment().hour("9").format("h a");
-var tenAmTxt = moment().hour("10").format("h a");
-var elevenAmTxt = moment().hour("11").format("h a");
-var twelvePmTxt = moment().hour("12").format("h a");
-var onePmTxt = moment().hour("13").format("h a");
-var twoPmTxt = moment().hour("14").format("h a");
-var threePmTxt = moment().hour("15").format("h a");
-var fourPmTxt = moment().hour("16").format("h a");
-var fivePmTxt = moment().hour("17").format("h a");
-var sixPmTxt = moment().hour("18").format("h a");
-var sevenPmTxt = moment().hour("19").format("h a");
-var eightPmTxt = moment().hour("20").format("h a");
-var ninePmTxt = moment().hour("21").format("h a");
-// NOTE: Using format("hha") compared the hour as the number
-// At 11am, todayHour = 11am. ninePmTxt.format('hha')=9pm
-// todayHour > ninePmTxt 
-// 11 > 9 --> TRUE. The time has passed. 
-// Want 11 > 21 --> FALSE. 
-
-// Set the hours for the time column
-// .hour lets you get or set the hour
-$('#h5Txt').text(fiveAmTxt);
-$('#h6Txt').text(sixAmTxt);
-$('#h7Txt').text(sevenAmTxt);
-$('#h8Txt').text(eightAmTxt);
-$('#h9Txt').text(nineAmTxt);
-$('#h10Txt').text(tenAmTxt);
-$('#h11Txt').text(elevenAmTxt);
-$('#h12Txt').text(twelvePmTxt);
-$('#h13Txt').text(onePmTxt);
-$('#h14Txt').text(twoPmTxt);
-$('#h15Txt').text(threePmTxt);
-// This could be made into a loop
-// May delete this so I'm not worried about formatting and I can get the formula to work
+var fiveAmTxt = moment().hour("5").format("HH"); 
+var sixAmTxt = moment().hour("6").format("HH");
+var sevenAmTxt = moment().hour("7").format("HH");
+var eightAmTxt = moment().hour("8").format("HH");
+var nineAmTxt = moment().hour("9").format("HH");
+var tenAmTxt = moment().hour("10").format("HH");
+var elevenAmTxt = moment().hour("11").format("HH");
+var twelvePmTxt = moment().hour("12").format("HH");
+var onePmTxt = moment().hour("13").format("HH");
+var twoPmTxt = moment().hour("14").format("HH");
+var threePmTxt = moment().hour("15").format("HH");
+var fourPmTxt = moment().hour("16").format("HH");
+var fivePmTxt = moment().hour("17").format("HH");
+var sixPmTxt = moment().hour("18").format("HH");
+var sevenPmTxt = moment().hour("19").format("HH");
+var eightPmTxt = moment().hour("20").format("HH");
+var ninePmTxt = moment().hour("21").format("HH");
 
 // CHALLENGE
 // var h = i+5
@@ -68,33 +47,30 @@ console.log(todayHour == ninePmTxt);
 
 
 function hour () {
-    // // Is the time in the past? 
-    // if (todayHour > fiveAmTxt) {
-    //     console.log("5: The time has passed")
-    //     $('#h-5-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
-    // // Is it the current time? 
-    // } else if (todayHour == fiveAmTxt) {
-    //     console.log("5: It's the current hour")
-    //     $('#h-5-form').children().children().eq(1).removeClass("past").addClass("present").removeClass("future");
-    // // Is the time in the future? 
-    // } else {
-    //     console.log("5: The time is in the future")
-    //     $('#h-5-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
-    // }
-
     // past: grey
     // current: red
     // future: green
     
     // Is the time in the past? 
+    if (todayHour > fiveAmTxt) {
+        console.log("6: The time has passed")
+        $('#h-5-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
+    // Is it the current time? 
+    } else if (todayHour == fiveAmTxt) {
+        console.log("6: It's the current hour")
+        $('#h-5-form').children().children().eq(1).removeClass("past").addClass("present").removeClass("future");
+    // Is the time in the future? 
+    } else {
+        console.log("6: The time is in the future")
+        $('#h-5-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
+    }
+
     if (todayHour > sixAmTxt) {
         console.log("6: The time has passed")
         $('#h-6-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
-    // Is it the current time? 
     } else if (todayHour == sixAmTxt) {
         console.log("6: It's the current hour")
         $('#h-6-form').children().children().eq(1).removeClass("past").addClass("present").removeClass("future");
-    // Is the time in the future? 
     } else {
         console.log("6: The time is in the future")
         $('#h-6-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
@@ -111,6 +87,54 @@ function hour () {
         $('#h-7-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
     }
 
+    if (todayHour > eightAmTxt) {
+        $('#h-8-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
+    } else if (todayHour == eightAmTxt) {
+        $('#h-8-form').children().children().eq(1).removeClass("past").addClass("present").removeClass("future");
+    } else {
+        $('#h-8-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
+    }
+
+    if (todayHour > nineAmTxt) {
+        $('#h-9-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
+    } else if (todayHour == nineAmTxt) {
+        $('#h-9-form').children().children().eq(1).removeClass("past").addClass("present").removeClass("future");
+    } else {
+        $('#h-9-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
+    }
+
+    if (todayHour > tenAmTxt) {
+        $('#h-10-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
+    } else if (todayHour == tenAmTxt) {
+        $('#h-10-form').children().children().eq(1).removeClass("past").addClass("present").removeClass("future");
+    } else {
+        $('#h-10-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
+    }
+
+    if (todayHour > elevenAmTxt) {
+        $('#h-11-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
+    } else if (todayHour == elevenAmTxt) {
+        $('#h-11-form').children().children().eq(1).removeClass("past").addClass("present").removeClass("future");
+    } else {
+        $('#h-11-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
+    }
+
+    if (todayHour > twelvePmTxt) {
+        $('#h-12-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
+    } else if (todayHour == twelvePmTxt) {
+        $('#h-12-form').children().children().eq(1).removeClass("past").addClass("present").removeClass("future");
+    } else {
+        $('#h-12-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
+    }
+
+    if (todayHour > onePmTxt) {
+        $('#h-13-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
+    } else if (todayHour == onePmTxt) {
+        $('#h-13-form').children().children().eq(1).removeClass("past").addClass("present").removeClass("future");
+    } else {
+        $('#h-13-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
+    }
+
     if (todayHour > twoPmTxt) {
         console.log("14: The time has passed")
         $('#h-14-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
@@ -122,12 +146,36 @@ function hour () {
         $('#h-14-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
     }
 
-    if (todayHour > ninePmTxt) {
-        console.log("The time has passed")
-    } else if (todayHour == ninePmTxt) {
-        console.log("It's the current hour")
+    if (todayHour > threePmTxt) {
+        $('#h-15-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
+    } else if (todayHour == threePmTxt) {
+        $('#h-15-form').children().children().eq(1).removeClass("past").addClass("present").removeClass("future");
     } else {
-        console.log("The time is in the future")
+        $('#h-15-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
+    }
+
+    if (todayHour > fourPmTxt) {
+        $('#h-16-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
+    } else if (todayHour == fourPmTxt) {
+        $('#h-16-form').children().children().eq(1).removeClass("past").addClass("present").removeClass("future");
+    } else {
+        $('#h-16-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
+    }
+
+    if (todayHour > fivePmTxt) {
+        $('#h-17-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
+    } else if (todayHour == fivePmTxt) {
+        $('#h-17-form').children().children().eq(1).removeClass("past").addClass("present").removeClass("future");
+    } else {
+        $('#h-17-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
+    }
+
+    if (todayHour > sixPmTxt) {
+        $('#h-18-form').children().children().eq(1).addClass("past").removeClass("present").removeClass("future");
+    } else if (todayHour == sixPmTxt) {
+        $('#h-18-form').children().children().eq(1).removeClass("past").addClass("present").removeClass("future");
+    } else {
+        $('#h-18-form').children().children().eq(1).removeClass("past").removeClass("present").addClass("future");
     }
 };
 
@@ -135,8 +183,6 @@ function hour () {
 hour();
 // Execute every 5 minutes
 setInterval(hour, 300000);
-
-
 
 
 // var test = $('#test').val();
@@ -153,6 +199,7 @@ testBtn.on('submit', testSubmit);
 
 
 // h-14-form
+var h14Value = $('input[name="h-14-value"]');
 var h14Btn = $('#h-14-form'); 
 
 // h-14 Submit
@@ -168,20 +215,34 @@ h14Btn.on('submit', h14Submit);
 
 // h-15-form
 var h15Value = $('input[name="h-15-value"]'); 
-var h15Btn = $('#h-15-form'); 
+var h15Btn = $('#h-15-form'); // button list
 
 // h-15 Submit
 function h15Submit(event) {
     event.preventDefault();
-    console.log("h15Submit: " + h15Value.val());
+    console.log("h15Submit: " + h15Value.val()); // not required
     localStorage.setItem("h15Local",h15Value.val());
 }
 
 h15Btn.on('submit', h15Submit);
 
 
-/// ----- Local Storage Recall ------
+// /// ----- Local Storage Recall ------
+// var timeBlocks = ['h5','h6', 'h7', 'h8', 'h14'];
+// var recallLi = [];
+// for (var i=0; i<timeBlocks.length; i++) {
+//     recallLi.push(timeBlocks[i] + "Recall"); 
+// }
+// console.log(recallLi); 
+// console.log(typeof recallLi); // object
+
+
+
+
+
 function LocalStorageRecall() {
+   
+
     // Test Local Storage Recall
     var testRecall = localStorage.getItem("testLocal")
     console.log("Test Recall: " + testRecall)
@@ -199,7 +260,7 @@ function LocalStorageRecall() {
     console.log("h15Value Recall: " + h15Recall)
     $('#h-15-form').children().children().eq(1).attr('value', h15Recall);
 
-
+    // BUILD TESTS :: Testing getData at different locations
     // $("#h-15-form") 
     // $('#test').text(testRecall);
     // $('#test3').text(testRecall);
@@ -210,4 +271,32 @@ function LocalStorageRecall() {
 // $("#button").on('click', localStorage.clear())
 
 
+// 1a. HTML create a primary button in HTML
+// 1b. Create an array of the time blocks
+// 1c. Create an array of the buttons
 
+// 2a. Event delegation calls the specific time block to locally store the data
+// 2b. Create var for the input data
+//     Use the data-time block to assign and name the variable
+
+// 3a. localStorageRecall
+// 3b. Create an array of the recall variable names
+// 3c. For loop to get the data from local storage
+//     Add attribute 'value' to <input> element    
+
+
+// var storedData = [
+//     h5Recall, 
+//     h6Recall, 
+//     h7Recall, 
+//     h8Recall,
+// ]
+
+// for (var i = 0) {
+
+// }; 
+
+// for (var i = 0; i < storedData.length; i++) {
+//     var storedData[i] = localStorage.getItem("h14local")
+//     $('#test-form').children().children().eq(1).attr('value', storedData[i]);
+// }
